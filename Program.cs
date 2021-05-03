85872051311a46eb4f8d89ff72c235035ff87263
Jordan Haywood <51302288+JordanHay@users.noreply.github.com>
2021-05-03 14:02:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            stopwatch.Start();
+
+            string stringOne = "Im sorry Dave.";
+            string stringTwo = "Im afraid I cant do that.";
+
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine($"{stringOne} {stringTwo}");
+            }
+
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken : { stopwatch.ElapsedMilliseconds } ms");
+
+
+            stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 500; i++)
+            {
+                stringBuilder.Append("I'm sorry Dave");
+            }
+
+            string sbOutput = stringBuilder.ToString();
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time taken : { stopwatch.ElapsedMilliseconds } ms");
+
+
+            Console.ReadLine();
+
+        }
+
+        
+
+   
+    }
+
+
+}
